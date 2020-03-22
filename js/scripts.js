06b6cf60bf74a2e1dc45c866fdf44e39f40df335
Cecilia Barasa <cecibarasa@gmail.com>
2020-03-22 10:46:01 +0300

    add conditions

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,18 @@ function getInput(){
     year = parseInt(document.getElementById("year").value);
     month = parseInt(document.getElementById("month").value);
     dayOfMonth = parseInt(document.getElementById("monthday").value);
+
+    if(century == ""){
+        alert("Input correct gender");
+        return false;
+    }else if (year == ""){
+        alert("Input correct year");
+        return false;
+    }else if (month == ""){
+        alert ("Input correct month");
+        return false;
+    }else if(dayOfMonth == ""){
+        alert("Input correct date");
+        return false;
+    }
 }
