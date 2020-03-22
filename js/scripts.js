c35f50e49e16ab304afaf3a2dc1594c70439aaf6
Cecilia Barasa <cecibarasa@gmail.com>
2020-03-22 10:36:20 +0300

    add input id in js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var century, year, month, dayOfMonth, day;
+//Get input
+function getInput(){
+    century = parseInt(document.getElementById("century").vaue);
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("month").value);
+    dayOfMonth = parseInt(document.getElementById("monthday").value);
+}
